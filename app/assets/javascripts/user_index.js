06a6ac87c44eedad9a6f8055155f3eea04cdf3fa
Gabriela Rotver <gabrielarotver86@gmail.com>
2016-07-31 11:23:12 -0400

    added jquery for zip code change

--- app/assets/javascripts/user_index.js
+++ app/assets/javascripts/user_index.js
@@ -1,19 +1,18 @@
 $(document).ready(function(){
   $('#enter_zip').click(function(){
+    // validate zip at some point!
     var zipCode = $('#zipInput').val();
     var key = "3ed8874ebfa166dd32b62e547f51ffca";
-
     var url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipCode + ",us&units=imperial&APPID=" + key;
 
     $.get(url, function(data){
-      console.log(data);
-
       var temp = data["main"]["temp"];
       var city = data["name"];
       var pic = data["weather"][0]["icon"];
-
-      console.log(temp + " " + city + " " + pic);
-
+      var icon_url = "http://openweathermap.org/img/w/" + pic + ".png"
+      $('span#city_name').text(city);
+      $('b#degrees').text(temp);
+      $('#weather_icon').attr("src", icon_url);
     });
   });
 
--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -4,27 +4,16 @@ class PageController < ApplicationController
 
   def user_index
     api_key = "3ed8874ebfa166dd32b62e547f51ffca"
-
     url = "http://api.openweathermap.org/data/2.5/weather?zip=33301,us&units=imperial&APPID=#{api_key}"
-
     res = HTTParty.get url
-
     response = JSON.parse(res.body)
     # puts response
-
     @temp = response["main"]["temp"]
-
     @city = response["name"]
-
-
     # @conditions = response["weather"][0]["description"].capitalize
 
-
     pic = response["weather"][0]["icon"]
-
     @icon_url = "http://openweathermap.org/img/w/#{pic}.png"
-
-
 
   end
 
